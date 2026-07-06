ae687f637d124d7679a98f7b57d4cc1de4d21876
dkzndk1100 <dkzndk1100@naver.com>
2026-07-06 23:20:17 +0900

    feat : 환경 감지용 컴포넌트 추가
    - 세부 기능 미정

---
+++ Client/Private/EnvironmentQueryComponent.cpp
@@ -0,0 +1,56 @@
+﻿#include "ClientPch.h"
+#include "EnvironmentQueryComponent.h"
+
+CEnvironmentQueryComponent::CEnvironmentQueryComponent(ID3D11Device* pDevice, ID3D11DeviceContext* pContext)
+	: CComponent { pDevice, pContext }
+{
+}
+
+CEnvironmentQueryComponent::CEnvironmentQueryComponent(const CEnvironmentQueryComponent& Prototype)
+	: CComponent ( Prototype )
+{
+}
+
+HRESULT CEnvironmentQueryComponent::Initialize_Prototype()
+{
+	return S_OK;
+}
+
+HRESULT CEnvironmentQueryComponent::Initialize_Clone(void* pArg)
+{
+	if (FAILED(__super::Initialize_Clone(pArg)))
+		return E_FAIL;
+
+	return S_OK;
+}
+
+CEnvironmentQueryComponent* CEnvironmentQueryComponent::Create(ID3D11Device* pDevice, ID3D11DeviceContext* pContext)
+{
+	CEnvironmentQueryComponent* pInstance = new CEnvironmentQueryComponent(pDevice, pContext);
+
+	if (FAILED(pInstance->Initialize_Prototype()))
+	{
+		MSG_BOX("Failed to Create : CEnvironmentQueryComponent");
+		Safe_Release(pInstance);
+	}
+
+	return pInstance;
+}
+
+Engine::CComponent* CEnvironmentQueryComponent::Clone(void* pArg)
+{
+	CEnvironmentQueryComponent* pClone = new CEnvironmentQueryComponent(*this);
+
+	if (FAILED(pClone->Initialize_Clone(pArg)))
+	{
+		MSG_BOX("Failed to Create : EnvironmentQueryComponent (Clone)");
+		Safe_Release(pClone);
+	}
+
+	return pClone;
+}
+
+void CEnvironmentQueryComponent::Free()
+{
+	__super::Free();
+}
---
+++ Client/Public/EnvironmentQueryComponent.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+#include "Component.h"
+
+NS_BEGIN(Client)
+class CEnvironmentQueryComponent final : public Engine::CComponent
+{
+protected:
+	explicit CEnvironmentQueryComponent(ID3D11Device* pDevice, ID3D11DeviceContext* pContext);
+	explicit CEnvironmentQueryComponent(const CEnvironmentQueryComponent& Prototype);
+	virtual ~CEnvironmentQueryComponent() = default;
+
+public:
+	virtual HRESULT		Initialize_Prototype() override;
+	virtual HRESULT		Initialize_Clone(void* pArg) override;
+
+public:
+	static	CEnvironmentQueryComponent* Create(ID3D11Device* pDevice, ID3D11DeviceContext* pContext);
+	virtual Engine::CComponent* Clone(void* pArg) override;
+	virtual void Free() override;
+};
+NS_END
+
