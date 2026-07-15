e5fdd83908947c1dd3cfd7ee0c6c1cf2b09d30dd
dkzndk1100 <dkzndk1100@naver.com>
2026-07-15 23:49:29 +0900

    feat : ClimbRun 회전을 보관할 컴포넌트

---
+++ Client/Private/MeshAlignComponent.cpp
@@ -0,0 +1,97 @@
+﻿#include "ClientPch.h"
+#include "MeshAlignComponent.h"
+
+CMeshAlignComponent::CMeshAlignComponent(ID3D11Device* pDevice, ID3D11DeviceContext* pContext)
+	: CComponent{ pDevice, pContext }
+{
+}
+
+CMeshAlignComponent::CMeshAlignComponent(const CMeshAlignComponent& Prototype)
+	: CComponent(Prototype)
+{
+}
+
+HRESULT CMeshAlignComponent::Initialize_Prototype()
+{
+	return S_OK;
+}
+
+HRESULT CMeshAlignComponent::Initialize_Clone(void* pArg)
+{
+	if (FAILED(__super::Initialize_Clone(pArg)))
+		return E_FAIL;
+
+	return S_OK;
+}
+
+void CMeshAlignComponent::Request_Pose(_fvector qRot, const _float3& vLocalOffset, _float fBlendTime)
+{
+	m_qStartRot    = m_qCurrentRot;
+	m_vStartOffset = m_vCurrentOffset;
+	XMStoreFloat4(&m_qTargetRot, XMQuaternionNormalize(qRot));
+	m_vTargetOffset  = vLocalOffset;
+	m_fBlendDuration = fBlendTime;
+	m_fBlendElapsed  = 0.f;
+
+	if (m_fBlendDuration <= 0.f)
+	{
+		m_qCurrentRot    = m_qTargetRot;
+		m_vCurrentOffset = m_vTargetOffset;
+	}
+}
+
+void CMeshAlignComponent::Clear_Pose(_float fBlendTime)
+{
+	Request_Pose(XMQuaternionIdentity(), _float3(0.f, 0.f, 0.f), fBlendTime);
+}
+
+void CMeshAlignComponent::Update(_float fTimeDelta)
+{
+	if (!IsBlending())
+		return;
+
+	m_fBlendElapsed = min(m_fBlendElapsed + fTimeDelta, m_fBlendDuration);
+	_float fRatio = m_fBlendElapsed / m_fBlendDuration;
+
+	XMStoreFloat4(&m_qCurrentRot, XMQuaternionSlerp(
+		XMLoadFloat4(&m_qStartRot), XMLoadFloat4(&m_qTargetRot), fRatio));
+	XMStoreFloat3(&m_vCurrentOffset, XMVectorLerp(
+		XMLoadFloat3(&m_vStartOffset), XMLoadFloat3(&m_vTargetOffset), fRatio));
+}
+
+_matrix CMeshAlignComponent::Get_LocalMatrix() const
+{
+	return XMMatrixRotationQuaternion(XMLoadFloat4(&m_qCurrentRot))
+		* XMMatrixTranslation(m_vCurrentOffset.x, m_vCurrentOffset.y, m_vCurrentOffset.z);
+}
+
+CMeshAlignComponent* CMeshAlignComponent::Create(ID3D11Device* pDevice, ID3D11DeviceContext* pContext)
+{
+	CMeshAlignComponent* pInstance = new CMeshAlignComponent(pDevice, pContext);
+
+	if (FAILED(pInstance->Initialize_Prototype()))
+	{
+		MSG_BOX("Failed to Create : MeshAlignComponent");
+		Safe_Release(pInstance);
+	}
+
+	return pInstance;
+}
+
+CComponent* CMeshAlignComponent::Clone(void* pArg)
+{
+	CMeshAlignComponent* pClone = new CMeshAlignComponent(*this);
+
+	if (FAILED(pClone->Initialize_Clone(pArg)))
+	{
+		MSG_BOX("Failed to Create : MeshAlignComponent (Clone)");
+		Safe_Release(pClone);
+	}
+
+	return pClone;
+}
+
+void CMeshAlignComponent::Free()
+{
+	__super::Free();
+}
---
+++ Client/Public/MeshAlignComponent.h
@@ -0,0 +1,41 @@
+﻿#pragma once
+#include "Component.h"
+
+NS_BEGIN(Client)
+class CMeshAlignComponent final : public Engine::CComponent
+{
+protected:
+	explicit CMeshAlignComponent(ID3D11Device* pDevice, ID3D11DeviceContext* pContext);
+	explicit CMeshAlignComponent(const CMeshAlignComponent& Prototype);
+	virtual ~CMeshAlignComponent() = default;
+
+public:
+	virtual HRESULT		Initialize_Prototype() override;
+	virtual HRESULT		Initialize_Clone(void* pArg) override;
+
+public:
+	void Request_Pose(_fvector qRot, const _float3& vLocalOffset, _float fBlendTime);
+	void Clear_Pose(_float fBlendTime);
+	void Update(_float fTimeDelta);
+
+	_matrix Get_LocalMatrix() const;
+	_bool   IsBlending() const { return m_fBlendElapsed < m_fBlendDuration; }
+
+private:
+	_float4 m_qStartRot   = { 0.f, 0.f, 0.f, 1.f };
+	_float4 m_qTargetRot  = { 0.f, 0.f, 0.f, 1.f };
+	_float4 m_qCurrentRot = { 0.f, 0.f, 0.f, 1.f };
+
+	_float3 m_vStartOffset   = {};
+	_float3 m_vTargetOffset  = {};
+	_float3 m_vCurrentOffset = {};
+
+	_float m_fBlendDuration = 0.f;
+	_float m_fBlendElapsed  = 0.f;
+
+public:
+	static	CMeshAlignComponent* Create(ID3D11Device* pDevice, ID3D11DeviceContext* pContext);
+	virtual Engine::CComponent* Clone(void* pArg) override;
+	virtual void Free() override;
+};
+NS_END
